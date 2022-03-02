7dd141f8ae7569592d588e6dea6fa49b8b4dcba7
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-02 11:27:02 -0300

    Atualizando Model

--- src/routes/servicos.js
+++ src/routes/servicos.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const ServicosController = require('../controllers/ServicosController')
 
-router.get('/',(req, res) => {
-  return res.render('servicos')
-});
+router.get('/', ServicosController.index);
 
-module.exports = router
+module.exports = router;
