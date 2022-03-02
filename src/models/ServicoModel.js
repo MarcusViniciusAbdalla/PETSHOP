930aab099dccf11228bf88a1fb87d62be16f51e3
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-02 11:27:02 -0300

    Atualizando Model

---
+++ src/models/ServicoModel.js
@@ -0,0 +1,31 @@
+module.exports = {
+  index: () => {
+    return [
+      {
+        nome: 'Banho' , 
+        valor: 'R$ 30,00' , 
+        descricao: 'Higienização Completa' 
+      },
+      { 
+        nome: 'Tosa' , 
+        valor: 'R$ 15,00' , 
+        descricao: 'Tosa Completa' 
+      },
+      { 
+        nome: 'Corte de Unhas' , 
+        valor: 'R$ 5,00' , 
+        descricao: 'Corte de unhas e lixar' 
+      },
+      {
+        nome: 'Hidratação' , 
+        valor: 'R$ 40,00' , 
+        descricao: 'Hidratação completa da pelagem' 
+      },
+      {
+        nome: 'Tingimento dos pêlos' , 
+        valor: 'R$ 100,00' , 
+        descricao: 'Tingimento toda pelagem' 
+      },
+    ]
+  }
+};
