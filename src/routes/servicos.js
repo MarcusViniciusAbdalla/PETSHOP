e4ac42c5cc5ac7b6b2cadc254c914e239c999dc1
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-02 23:48:00 -0300

    Alterações

--- src/routes/servicos.js
+++ src/routes/servicos.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ServicosController = require('../controllers/ServicosController')
 
 router.get('/', ServicosController.index);
+router.get('/admin' , ServicosController.admin);
 
 module.exports = router;
