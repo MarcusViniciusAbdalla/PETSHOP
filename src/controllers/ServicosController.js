af2043adeb80c83baea56dd8c2ca8234bd5b5d44
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-02 23:48:00 -0300

    Alterações

--- src/controllers/ServicosController.js
+++ src/controllers/ServicosController.js
@@ -2,7 +2,14 @@ const ServicoModel = require('../models/ServicoModel');
 
 module.exports = {
   index: (req, res) => {
+    const servicos = ServicoModel.index(); // <-----Pedindo os dados para o modelo
+    
+    return res.render('servicos', { servicos }); // ------> Enviando os dados para a view
+  },
+
+  admin:(req,res) => {
     const servicos = ServicoModel.index();
-    return res.render('servicos', { servicos });
+    return res.render('admin-servicos', { servicos });
   }
 };
+
