7a47de3d8342aa89c151d57fc3fc74bd6fda317e
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-04 12:32:39 -0300

    Atualizando Multer e

--- src/routes/servicos.js
+++ src/routes/servicos.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const ServicosController = require('../controllers/ServicosController');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null , 'public/images/uploads')
+  },
+
+  filename(req, file, callback) {
+    const [filename, extension] = file.originalname.split('.'); // [ banho , jpeg ]
+    callback(null , filename + '-'+ Date.now() + '.' + extension)  //banho 20210222.jpeg
+  }
+})
 
 // C - Criação de novos serviços
 router.get('/cadastrar', ServicosController.editar);
