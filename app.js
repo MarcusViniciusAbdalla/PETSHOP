286131e0c27c844fea01835debef7ef9ca67a11f
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-05-27 11:25:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
 var petsRouter = require('./src/routes/pets');
 var servicosRouter = require('./src/routes/servicos');
+var sobreRouter = require('./src/routes/sobre');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pets', petsRouter);
 app.use('/servicos', servicosRouter);
+app.use('/sobre', sobreRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
